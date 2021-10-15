a0862278a3d0985506f54be46ea9ef0b48f47a82
ptlharshil <patelharshil3695@gmail.com>
2021-10-14 22:35:15 -0400

    Home page modified

--- script.js
+++ script.js
@@ -1,7 +1,1 @@
-var homeCard=document.getElementById('homeSection');
-
-window.addEventListener('scroll', function(){
-    let value=this.window.scrollY;
-    homeCard.style.marginRight=value*4+'px';
-    homeCard.style.marginTop=value*1.5+'px';
-})
+const swup=new Swup();
